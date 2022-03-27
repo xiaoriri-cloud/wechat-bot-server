b6f7c8e6b421a988c33f229034616045de16f5ab
xiaoriri <admin@xiaoriri.com>
2022-03-27 20:01:02 +0800

    bushu

--- main.go
+++ main.go
@@ -6,7 +6,7 @@ func main() {
 	r := gin.Default()
 	r.GET("/ping", func(c *gin.Context) {
 		c.JSON(200, gin.H{
-			"message": "无语了？？？？123231123",
+			"message": "asadsda无语了？？？？123231123",
 		})
 	})
 	r.Run() // 监听并在 0.0.0.0:8080 上启动服务
